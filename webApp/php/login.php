1be2d53e51bebba6096629746636fcd81d57de0d
Jose <106976824+JoseeMB@users.noreply.github.com>
2024-10-12 11:03:30 -0300

    Jose (#2)
    * Login And web

---
+++ webApp/php/login.php
@@ -0,0 +1,27 @@
+<?php
+$db = pg_connect("host=localhost port=5432 dbname=grupo80 user=grupo80 password=grupo80");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Verificar si el usuario es "bananer@lamejor.com" con clave "bananer0"
+    if ($email === 'bananer@lamejor.com' && $password === 'bananer0') {
+        // Redirigir a la interfaz de añadir usuarios
+        header("Location: ../views/add_user_interface.html");
+        exit();
+    }
+
+    // Consulta para verificar las credenciales en la base de datos
+    $query = "SELECT * FROM users WHERE email = $1 AND password = $2";
+    $result = pg_query_params($db, $query, array($email, $password));
+
+    if (pg_num_rows($result) > 0) {
+        // Credenciales correctas, redirigir al menú principal
+        header("Location: ../views/menu_principal.html");
+        exit();
+    } else {
+        // Credenciales incorrectas, mostrar alerta
+        echo "<script>alert('Invalid email or password.'); window.location.href='../views/login.html';</script>";
+    }
+}
+?>
