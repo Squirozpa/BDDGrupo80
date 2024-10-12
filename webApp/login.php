626a936b67a586a37a297a6760b2e042fc847ba4
jorojojojo23 <jbarres@uc.cl>
2024-10-11 22:28:42 -0300

    create users

--- webApp/login.php
+++ webApp/login.php
@@ -1,22 +1,18 @@
 <?php
-// Conexión a la base de datos
 $db = pg_connect("host=localhost port=5432 dbname=grupo80 user=grupo80 password=grupo80");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Capturar datos del formulario
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Consulta para verificar las credenciales
     $query = "SELECT * FROM users WHERE email = $1 AND password = $2";
     $result = pg_query_params($db, $query, array($email, $password));
 
     if (pg_num_rows($result) > 0) {
-        // Credenciales correctas
-        echo "Login successful!";
+        header("Location: menu.html");
+        exit();
     } else {
-        // Credenciales incorrectas
-        echo "Invalid email or password.";
+        echo "<script>alert('Invalid email or password.'); window.location.href='login.html';</script>";
     }
 }
 ?>
