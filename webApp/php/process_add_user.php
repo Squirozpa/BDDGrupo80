85242fd246bed2911a1c82ac4e220924179857b2
jorojojojo23 <jbarres@uc.cl>
2024-10-16 21:44:33 -0300

    Load All

--- webApp/php/process_add_user.php
+++ webApp/php/process_add_user.php
@@ -15,12 +15,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     // Verificar si el usuario ya existe
-    $check_query = "SELECT * FROM users WHERE email = $1";
-    $check_result = pg_query_params($db, $check_query, array($email));
+    $new_user_query = "SELECT * FROM users WHERE email = $1";
+    $new_user_result = pg_query_params($db, $new_user_query, array($email));
 
-    if (pg_num_rows($check_result) > 0) {
+    $docente_valid_query = "SELECT email_institucional FROM docentes WHERE email_institucional = $1";
+    $docente_valid_result = pg_query_params($db, $docente_valid_query, array($email));
+
+    if (pg_num_rows($new_user_result) > 0) {
         echo "<script>alert('El usuario ya existe.'); window.location.href='../views/add_user_interface.html';</script>";
-    } else {
+    } elseif(pg_num_rows($docente_valid_result) == 0){
+        echo "<script>alert('El usuario no es parte de la institución.'); window.location.href='../views/add_user_interface.html';</script>";
+    }
+    else {
         // Insertar el nuevo usuario en la base de datos
         $query = "INSERT INTO users (email, password) VALUES ($1, $2)";
         $result = pg_query_params($db, $query, array($email, $password));
