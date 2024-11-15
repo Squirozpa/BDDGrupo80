33d09a650195d2df622d6cc867df6281d684c172
jorojojojo23 <jbarres@uc.cl>
2024-11-14 22:33:34 -0300

    Update desde tabla de profesores

--- testDBConn.php
+++ testDBConn.php
@@ -1,5 +1,5 @@
 <?php
 $db = pg_connect("host=localhost port=5432 dbname=grupo80e3 user=grupo80e3 password=grupo80");
-$db_profes = pg_connect("host=localhost port=5432 dbname=e3profesores user=grupo80e3 password=contraseña");
+$db_profes = pg_connect("host=localhost port=5432 dbname=e3profesores user=grupo80e3 password=grupo80");
 
 ?>
