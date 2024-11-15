9799e55bad52ca7bcc1aa35af040573f7bc42f39
jorojojojo23 <jbarres@uc.cl>
2024-11-14 22:33:52 -0300

    Update desde tabla de profesores

--- UpdatePersonas.php
+++ UpdatePersonas.php
@@ -1,6 +1,6 @@
 <?php
 $db = pg_connect("host=localhost port=5432 dbname=grupo80e3 user=grupo80e3 password=grupo80");
-$db_profes = pg_connect("host=localhost port=5432 dbname=e3profesores user=grupo80e3 password=contraseña");
+$db_profes = pg_connect("host=localhost port=5432 dbname=e3profesores user=grupo80e3 password=grupo80");
 
 if (!$db || !$db_profes) {
     echo "Error: Unable to open database.\n";
