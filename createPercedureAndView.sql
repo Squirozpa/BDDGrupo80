8365c3532a35cd83946f48fadb16493afcdf810d
jorojojojo23 <jbarres@uc.cl>
2024-11-16 23:12:28 -0300

    CreatTempTable

--- createPercedureAndView.sql
+++ createPercedureAndView.sql
@@ -1,16 +1,16 @@
--- Crear el Stored Procedure
 CREATE OR REPLACE FUNCTION crear_acta()
 RETURNS VOID AS $$
 BEGIN
-    -- Crear la Vista
     CREATE OR REPLACE VIEW acta_notas AS
     SELECT 
         a.numero_alumno,
         a.run,
         a.sigla AS curso,
         a.seccion,
-        a.periodo
-        a.nota_final
-    FROM acta a;
+        a.periodo,
+        e.primer_nombre AS nombre_estudiante,
+        a.nota AS nota_final
+    FROM acta a
+    LEFT JOIN estudiantes e ON a.numero_alumno = e.numero_alumno;
 END;
 $$ LANGUAGE plpgsql;
--- createTempTable.php
+++ createTempTable.php
@@ -105,6 +105,7 @@ if (($handle = fopen($archivo, "r")) !== FALSE) {
                     echo "Curso: " . $row['curso'] . "\n";
                     echo "Seccion: " . $row['seccion'] . "\n";
                     echo "Periodo: " . $row['periodo'] . "\n";
+                    echo "Nombre Estudiante: " . $row['nombre_estudiante'] . "\n";
                     echo "Nota Final: " . $row['nota_final'] . "\n";
                     echo "-------------------------\n";
                 }
