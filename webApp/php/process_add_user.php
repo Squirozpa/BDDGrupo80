f6473fbdef736dfee345c2b33bb0dc43d105745d
squirozpa <squirozpa@uc.cl>
2024-11-18 20:54:46 -0300

    Update user create

--- webApp/php/process_add_user.php
+++ webApp/php/process_add_user.php
@@ -9,23 +9,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $password = $_POST['password'];
 
     if (!preg_match('/^[a-zA-Z0-9]{8}$/', $password)) {
-        echo "<script>alert('La contraseña deben ser 8 alfanumericos.'); window.location.href='../views/add_user_interface.html';</script>";
+        echo "<script>alert('La contraseña debe tener exactamente 8 caracteres alfanuméricos.'); window.location.href='../views/add_user_interface.html';</script>";
         exit;
     }
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
+
+    if (substr($email, -6) !== '@uc.cl') {
+        echo "<script>alert('El correo electrónico debe terminar con @uc.cl.'); window.location.href='../views/add_user_interface.html';</script>";
+        exit;
+    }
+
     $new_user_query = "SELECT * FROM users WHERE email = $1";
     $new_user_result = pg_query_params($db, $new_user_query, array($email));
 
-    $docente_valid_query = "SELECT email_institucional FROM docentes WHERE email_institucional = $1";
-    $docente_valid_result = pg_query_params($db, $docente_valid_query, array($email));
-
     if (pg_num_rows($new_user_result) > 0) {
         echo "<script>alert('El usuario ya existe.'); window.location.href='../views/add_user_interface.html';</script>";
-    } elseif(pg_num_rows($docente_valid_result) == 0){
-        echo "<script>alert('El usuario no es parte de la institución.'); window.location.href='../views/add_user_interface.html';</script>";
-    }
-    else {
+    } else {
         $query = "INSERT INTO users (email, password) VALUES ($1, $2)";
         $result = pg_query_params($db, $query, array($email, $hashed_password));
 
