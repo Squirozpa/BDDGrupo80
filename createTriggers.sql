16d8ebda41417ed086b269ad54c4faf539a0425d
cristobalnic <cristobalnic@uc.cl>
2024-11-18 19:58:32 -0300

    fix(Trigger): Change 'calif' to 'calificacion'

--- createTriggers.sql
+++ createTriggers.sql
@@ -1,21 +1,21 @@
-CREATE OR REPLACE FUNCTION calcular_calif() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION calcular_calificacion() RETURNS TRIGGER AS $$
 BEGIN
     IF NEW.nota IS NULL THEN
-        NEW.calif := NULL;
+        NEW.calificacion := NULL;
     ELSIF NEW.nota >= 6.6 AND NEW.nota <= 7.0 THEN
-        NEW.calif := 'SO'; -- Sobresaliente
+        NEW.calificacion := 'SO'; -- Sobresaliente
     ELSIF NEW.nota >= 6.0 AND NEW.nota < 6.6 THEN
-        NEW.calif := 'MB'; -- Muy Bueno
+        NEW.calificacion := 'MB'; -- Muy Bueno
     ELSIF NEW.nota >= 5.0 AND NEW.nota < 6.0 THEN
-        NEW.calif := 'B';  -- Bueno
+        NEW.calificacion := 'B';  -- Bueno
     ELSIF NEW.nota >= 4.0 AND NEW.nota < 5.0 THEN
-        NEW.calif := 'SU'; -- Suficiente
+        NEW.calificacion := 'SU'; -- Suficiente
     ELSIF NEW.nota >= 3.0 AND NEW.nota < 4.0 THEN
-        NEW.calif := 'I';  -- Insuficiente
+        NEW.calificacion := 'I';  -- Insuficiente
     ELSIF NEW.nota >= 2.0 AND NEW.nota < 3.0 THEN
-        NEW.calif := 'M';  -- Malo
+        NEW.calificacion := 'M';  -- Malo
     ELSIF NEW.nota >= 1.0 AND NEW.nota < 2.0 THEN
-        NEW.calif := 'MM'; -- Muy Malo
+        NEW.calificacion := 'MM'; -- Muy Malo
     END IF;
     RETURN NEW;
 END;
@@ -24,4 +24,4 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER set_calificacion
 BEFORE INSERT ON notas
 FOR EACH ROW
-EXECUTE FUNCTION calcular_calif();
+EXECUTE FUNCTION calcular_calificacion();
