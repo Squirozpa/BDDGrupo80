15c928d53d266a0211f0d45b8927aae2841b0c44
cristobalnic <cristobalnic@uc.cl>
2024-11-18 19:49:03 -0300

    fix(Trigger): Edit to match psql requirements

--- createTriggers.sql
+++ createTriggers.sql
@@ -1,43 +1,27 @@
-CREATE TRIGGER calcular_calificacion
+CREATE OR REPLACE FUNCTION calcular_calif() RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.nota IS NULL THEN
+        NEW.calif := NULL;
+    ELSIF NEW.nota >= 6.6 AND NEW.nota <= 7.0 THEN
+        NEW.calif := 'SO'; -- Sobresaliente
+    ELSIF NEW.nota >= 6.0 AND NEW.nota < 6.6 THEN
+        NEW.calif := 'MB'; -- Muy Bueno
+    ELSIF NEW.nota >= 5.0 AND NEW.nota < 6.0 THEN
+        NEW.calif := 'B';  -- Bueno
+    ELSIF NEW.nota >= 4.0 AND NEW.nota < 5.0 THEN
+        NEW.calif := 'SU'; -- Suficiente
+    ELSIF NEW.nota >= 3.0 AND NEW.nota < 4.0 THEN
+        NEW.calif := 'I';  -- Insuficiente
+    ELSIF NEW.nota >= 2.0 AND NEW.nota < 3.0 THEN
+        NEW.calif := 'M';  -- Malo
+    ELSIF NEW.nota >= 1.0 AND NEW.nota < 2.0 THEN
+        NEW.calif := 'MM'; -- Muy Malo
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_calificacion
 BEFORE INSERT ON notas
 FOR EACH ROW
-BEGIN
-    DECLARE calif VARCHAR(2);
-    DECLARE resultado VARCHAR(20);
-    
-    -- Asignación de calificación y resultado basado en la nota
-    IF NEW.nota >= 6.6 AND NEW.nota <= 7.0 THEN
-        SET calif = 'SO';
-    ELSEIF NEW.nota >= 6.0 AND NEW.nota < 6.6 THEN
-        SET calif = 'MB';
-    ELSEIF NEW.nota >= 5.0 AND NEW.nota < 6.0 THEN
-        SET calif = 'B';
-    ELSEIF NEW.nota >= 4.0 AND NEW.nota < 5.0 THEN
-        SET calif = 'SU';
-    ELSEIF NEW.nota >= 3.0 AND NEW.nota < 4.0 THEN
-        SET calif = 'I';
-    ELSEIF NEW.nota >= 2.0 AND NEW.nota < 3.0 THEN
-        SET calif = 'M';
-    ELSEIF NEW.nota >= 1.0 AND NEW.nota < 2.0 THEN
-        SET calif = 'MM';
-    ELSEIF NEW.nota IS NULL THEN
-        -- Notas nulas con resultado según tipo de nota especial
-        IF NEW.oportunidad = 'P' THEN
-            SET calif = 'P';
-        ELSEIF NEW.oportunidad = 'NP' THEN
-            SET calif = 'NP';
-        ELSEIF NEW.oportunidad = 'EX' THEN
-            SET calif = 'EX';
-        ELSEIF NEW.oportunidad = 'A' THEN
-            SET calif = 'A';
-        ELSEIF NEW.oportunidad = 'R' THEN
-            SET calif = 'R';
-        ELSE
-            SET calif = NULL;
-        END IF;
-    END IF;
-
-    -- Asignar calificación y resultado calculados al nuevo registro
-    SET NEW.calificacion = calif;
-    SET NEW.resultado = resultado;
-END;
+EXECUTE FUNCTION calcular_calif();
