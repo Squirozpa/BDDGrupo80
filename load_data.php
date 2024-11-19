fdb4a0822e481ee255f20a48120993ba175b7ad7
squirozpa <squirozpa@uc.cl>
2024-11-19 00:20:56 -0300

    validacion fix y csv

--- load_data.php
+++ load_data.php
@@ -43,7 +43,7 @@ foreach ($clean_malla as $key => $row) {
 
 }
 
-$filename = 'E2_prereq.csv';
+$filename = '../../E2_prereq.csv';
 if (($handle = fopen($filename, 'r')) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $unclean_prereq[] = $data;
