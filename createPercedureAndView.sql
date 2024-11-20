00219c9640ab0b22bb44e36429170ad2b6ade9c4
jorojojojo23 <jbarres@uc.cl>
2024-11-19 22:58:02 -0300

    A

--- createPercedureAndView.sql
+++ createPercedureAndView.sql
@@ -1,13 +1,18 @@
--- Function to create the view
 CREATE OR REPLACE FUNCTION crear_acta()
 RETURNS VOID AS $$
 BEGIN
-    -- Drop the view if it exists
-    IF EXISTS (SELECT 1 FROM pg_views WHERE viewname = 'vista_acta') THEN
-        DROP VIEW IF EXISTS vista_acta;
-    END IF;
 
-    -- Create the view
-    CREATE VIEW vista_acta AS SELECT numero_alumno, run, curso, seccion, periodo, nota FROM acta;
+    CREATE OR REPLACE VIEW acta_notas AS
+    SELECT 
+        a.numero_alumno,
+        a.run,
+        a.sigla AS curso,
+        a.seccion,
+        a.periodo,
+        a.nota,
+        a.calificacion
+    FROM acta a
+
+    PERFORM * FROM acta_notas;
 END;
 $$ LANGUAGE plpgsql;
--- createTempTable.php
+++ createTempTable.php
@@ -90,6 +90,27 @@ if (($handle = fopen($archivo, "r")) !== FALSE) {
         // Mostrar el contenido de la tabla temporal
         $query = pg_query($db, "SELECT crear_acta();");
 
+        if (!$query) {
+            echo "Error creating view: " . pg_last_error($db) . "\n";
+        } else {
+            // Mostrar el contenido de la vista
+            $query_view = pg_query($db, "SELECT * FROM acta_notas");
+
+            if (!$query_view) {
+                echo "Error fetching data from acta_notas: " . pg_last_error($db) . "\n";
+            } else {
+                while ($row = pg_fetch_assoc($query_view)) {
+                    echo "Numero Alumno: " . $row['numero_alumno'] . "<br>";
+                    echo "RUN: " . $row['run'] . "<br>";
+                    echo "Curso: " . $row['curso'] . "<br>";
+                    echo "Seccion: " . $row['seccion'] . "<br>";
+                    echo "Periodo: " . $row['periodo'] . "<br>";
+                    echo "Nota Final: " . $row['nota'] . "<br>";
+                    echo "Calificación: " . $row['calificacion'] . "<br>";
+                    echo "-------------------------<br>";
+                }
+            }
+        }
     }
 } else {
     echo "Error: Unable to open file $archivo.\n";
