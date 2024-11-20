179924745a48e42a969dc95e2b68e8167a740efc
jorojojojo23 <jbarres@uc.cl>
2024-11-19 23:10:50 -0300

    A

--- createPercedureAndView.sql
+++ createPercedureAndView.sql
@@ -1,16 +1,7 @@
 CREATE OR REPLACE FUNCTION crear_acta()
 RETURNS VOID AS $$
 BEGIN
-
     CREATE OR REPLACE VIEW acta_notas AS
-    SELECT 
-        numero_alumno,
-        run,
-        sigla AS curso,
-        seccion,
-        periodo,
-        nota,
-        calificacion
-    FROM acta
+    SELECT * FROM acta
 END;
 $$ LANGUAGE plpgsql;
