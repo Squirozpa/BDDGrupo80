f4d787e9a5654ebe70d3459af0addc5e0f658304
jorojojojo23 <jbarres@uc.cl>
2024-11-19 22:37:25 -0300

    A

--- createTempTable.php
+++ createTempTable.php
@@ -67,7 +67,7 @@ if (($handle = fopen($archivo, "r")) !== FALSE) {
         }
 
         // Insertar en la tabla temporal
-        $insert_query = "INSERT INTO acta (numero_alumno, run, sigla, seccion, periodo, nota) VALUES ('$numero_alumno', '$run', '$sigla','$seccion', '$periodo', $nota_final)";
+        $insert_query = "INSERT INTO acta (numero_alumno, run, sigla, seccion, periodo, nota) VALUES ('$numero_alumno', '$run', '$sigla','$seccion', '$periodo', '$nota_final')";
         $result = pg_query($db, $insert_query);
 
         if (!$result) {
