87a842ff2fe80ee056963beae7e9451d5cadc693
jorojojojo23 <jbarres@uc.cl>
2024-11-19 22:50:15 -0300

    A

--- createPercedureAndView.sql
+++ createPercedureAndView.sql
@@ -1,23 +1,13 @@
+-- Function to create the view
 CREATE OR REPLACE FUNCTION crear_acta()
 RETURNS VOID AS $$
 BEGIN
-        CREATE OR REPLACE VIEW vista_acta AS
-        SELECT numero_alumno, run, curso, seccion, periodo, nota
-        FROM acta;
+    -- Drop the view if it exists
+    IF EXISTS (SELECT 1 FROM pg_views WHERE viewname = 'vista_acta') THEN
+        DROP VIEW IF EXISTS vista_acta;
+    END IF;
 
-        -- Function to create the view
-        CREATE OR REPLACE FUNCTION crear_acta()
-        RETURNS VOID AS $$
-        BEGIN
-            -- Drop the view if it exists
-            IF EXISTS (SELECT 1 FROM pg_views WHERE viewname = 'vista_acta') THEN
-                EXECUTE 'DROP VIEW vista_acta';
-            END IF;
-
-            -- Create the view
-            EXECUTE 'CREATE VIEW vista_acta AS SELECT numero_alumno, run, curso, seccion, periodo, nota FROM acta';
-        END;
-        $$ LANGUAGE plpgsql;
-    
+    -- Create the view
+    CREATE VIEW vista_acta AS SELECT numero_alumno, run, curso, seccion, periodo, nota FROM acta;
 END;
 $$ LANGUAGE plpgsql;
