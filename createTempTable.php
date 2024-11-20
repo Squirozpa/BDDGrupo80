0423357b4559ff6e307c74c2aa6dbaddf8e1a3ae
jorojojojo23 <jbarres@uc.cl>
2024-11-19 22:30:42 -0300

    A

--- createTempTable.php
+++ createTempTable.php
@@ -100,12 +100,13 @@ if (($handle = fopen($archivo, "r")) !== FALSE) {
                 echo "Error fetching data from acta_notas: " . pg_last_error($db) . "\n";
             } else {
                 while ($row = pg_fetch_assoc($query_view)) {
+                    $nota = "SELECT nota FROM acta WHERE run = '$run' AND sigla = '$sigla' AND seccion = '$seccion' AND periodo = '$periodo'";
                     echo "Numero Alumno: " . $row['numero_alumno'] . "<br>";
                     echo "RUN: " . $row['run'] . "<br>";
                     echo "Curso: " . $row['curso'] . "<br>";
                     echo "Seccion: " . $row['seccion'] . "<br>";
                     echo "Periodo: " . $row['periodo'] . "<br>";
-                    echo "Nota Final: " . $row['nota'] . "<br>";
+                    echo "Nota Final: " . $nota . "<br>";
                     echo "-------------------------<br>";
                 }
             }
